2496a5a6592450f986ee3d94cc36495e80ee2dde
Jake Bender <jdbender64@gmail.com>
2024-07-06 20:40:33 -0700

    fixes rest of error handling issues

--- src/controllers/errorController.cjs
+++ src/controllers/errorController.cjs
@@ -1,3 +1,22 @@
+const AppError = require("../utils/appError.cjs");
+
+const handleDBCastError = (err) => {
+    const message = `Invalid ${err.path}: ${err.value}.`;
+    return new AppError(message, 400);
+};
+
+const handleDBDuplicateFieldsError = (err) => {
+    const message = `Duplicate field value(s): ${Object.keys(err.keyValue)}. Please use unique values.`;
+    return new AppError(message, 400);
+};
+
+const handleDBValidationError = (err) => {
+    const errors = Object.values(err.errors).map((el) => el.message);
+
+    const message = `Invalid input data. ${errors.join(". ")}`;
+    return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -27,7 +46,17 @@ const globalErrorHandler = (err, req, res, next) => {
     if (process.env.NODE_ENV === "development") {
         sendErrorDev(err, res);
     } else if (process.env.NODE_ENV === "production") {
-        sendErrorProd(err, res);
+        let error = { ...err };
+
+        if (error.name === "CastError") {
+            error = handleDBCastError(error);
+        } else if (error.code === 11000) {
+            error = handleDBDuplicateFieldsError(error);
+        } else if (error.name === "ValidationError") {
+            error = handleDBValidationError(error);
+        }
+
+        sendErrorProd(error, res);
     }
 
     next();
