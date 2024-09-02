3c4986e257c7532d6eba16ee366827915bce9fb8
Jake Bender <jdbender64@gmail.com>
2024-09-02 15:53:00 -0700

    improves file structure

---
+++ src/controllers/userController.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+
+export const getAllUsers = (req: Request, res: Response) => {
+    res.status(500).json({
+        status: "error",
+        message: "Route not yet created.",
+    });
+};
+
+export const createUser = (req: Request, res: Response) => {
+    res.status(500).json({
+        status: "error",
+        message: "Route not yet created.",
+    });
+};
+
+export const getUser = (req: Request, res: Response) => {
+    res.status(500).json({
+        status: "error",
+        message: "Route not yet created.",
+    });
+};
+
+export const updateUser = (req: Request, res: Response) => {
+    res.status(500).json({
+        status: "error",
+        message: "Route not yet created.",
+    });
+};
+
+export const deleteUser = (req: Request, res: Response) => {
+    res.status(500).json({
+        status: "error",
+        message: "Route not yet created.",
+    });
+};
