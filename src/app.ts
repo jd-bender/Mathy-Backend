291c23ccd76d6b49cb6dcfbd978b0ee20ed5d2a2
Jake Bender <jdbender64@gmail.com>
2024-09-02 14:02:10 -0700

    adds user route callbacks

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,55 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+const responseCallback = (req: express.Request, res: express.Response) => {
+    res.status(200).json({ message: "This is from the server" });
+};
+
+app.route("/").get(responseCallback);
+
+const getAllUsers = (req: express.Request, res: express.Response) => {
+    res.status(500).json({
+        status: "error",
+        message: "Route not yet created.",
+    });
+};
+
+const createUser = (req: express.Request, res: express.Response) => {
+    res.status(500).json({
+        status: "error",
+        message: "Route not yet created.",
+    });
+};
+
+app.route("/users").get(getAllUsers).post(createUser);
+
+const getUser = (req: express.Request, res: express.Response) => {
+    res.status(500).json({
+        status: "error",
+        message: "Route not yet created.",
+    });
+};
+
+const updateUser = (req: express.Request, res: express.Response) => {
+    res.status(500).json({
+        status: "error",
+        message: "Route not yet created.",
+    });
+};
+
+const deleteUser = (req: express.Request, res: express.Response) => {
+    res.status(500).json({
+        status: "error",
+        message: "Route not yet created.",
+    });
+};
+
+app.route("/users/:id").get(getUser).patch(updateUser).delete(deleteUser);
 
 const port = 3001;
 
