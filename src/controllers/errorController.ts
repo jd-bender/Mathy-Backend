f4ef4ee57d58c05af25d8559f42d9470732ee6aa
Jake Bender <jdbender64@gmail.com>
2024-09-03 03:04:03 -0700

    adds improved error handling

--- src/controllers/errorController.ts
+++ src/controllers/errorController.ts
@@ -1,15 +1,35 @@
 import { Request, Response, NextFunction } from "express";
 
 interface Error {
-    statusCode?: number;
+    statusCode: number;
+    status?: string;
     message?: string;
+    stack?: Array<string>;
 }
+
+const sendErrorDev = (e: Error, res: Response) => {
+    res.status(e.statusCode).json({
+        status: e.status,
+        error: e,
+        message: e.message,
+        stack: e.stack,
+    });
+};
+
+const sendErrorProd = (e: Error, res: Response) => {
+    res.status(e.statusCode).json({
+        status: e.status,
+        message: e.message,
+    });
+};
 
 export default (e: Error, req: Request, res: Response, next: NextFunction) => {
     e.statusCode = e.statusCode || 500;
+    e.status = e.status || "error";
 
-    res.status(e.statusCode).json({
-        status: "error",
-        message: e.message,
-    });
+    if (process.env.NODE_ENV === "development") {
+        sendErrorDev(e, res);
+    } else if (process.env.NODE_ENV === "production") {
+        sendErrorProd(e, res);
+    }
 };
